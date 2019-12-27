317cc87b38ac3c5254b36e29fe556d64f72d9ec7
Nitai Charan <nitaicharan@gmail.com>
2019-12-27 20:59:19 -0300

    40. Flutter Layouts Challenge
    - Row
    - Column
    - Container
    - SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() => (
-  runApp(MyApp())
-);
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
- Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black87,
-        body: Container()
-      ),
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.red,
+                ),
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                          color: Colors.yellow, width: 100.0, height: 100.0),
+                      Container(
+                          color: Colors.green, width: 100.0, height: 100.0),
+                    ]),
+                Container(
+                  color: Colors.blue,
+                  height: 100.0,
+                  width: 100.0,
+                )
+              ],
+            ),
+          )),
     );
   }
 }
