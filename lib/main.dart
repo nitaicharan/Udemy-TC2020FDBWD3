8a658b1c2b3836c1bf4f24822b9f9c814dd2339a
Nitai Charan <nitaicharan@gmail.com>
2019-12-27 22:19:59 -0300

    41. Tapping into Widget Properties
    - CircleAvatar
    - FontWeight
    - TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,30 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.teal,
-          body: SafeArea(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  height: 100.0,
-                  width: 100.0,
-                  color: Colors.red,
-                ),
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                          color: Colors.yellow, width: 100.0, height: 100.0),
-                      Container(
-                          color: Colors.green, width: 100.0, height: 100.0),
-                    ]),
-                Container(
-                  color: Colors.blue,
-                  height: 100.0,
-                  width: 100.0,
-                )
-              ],
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/profile.jpeg'),
             ),
-          )),
+            Text(
+              'Nitai Charan',
+              style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ]),
+        ),
+      ),
     );
   }
 }
